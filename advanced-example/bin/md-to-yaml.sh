566d85526123018de8b3d2343bd579d0a4e55eb6
Heechul Ryu <ryuhcii@gmail.com>
2022-03-03 16:26:59 -0800

    Fix advanced-example/bin/md-to-yaml.sh

--- advanced-example/bin/md-to-yaml.sh
+++ advanced-example/bin/md-to-yaml.sh
@@ -3,5 +3,7 @@
 md_file="${1}"
 
 # requires `yarn global add parse-markdown-table-cli`
-yq 'with(.[].0repo; . |= sub("\[(.*)].*\)","$1"))' out/data.yaml \
-	| parse-markdown-table
+cat "${md_file}" \
+	| parse-markdown-table \
+	| yq 'with(.[].0repo; . |= sub("\[(.*)].*\)","$1"))' -P
+
